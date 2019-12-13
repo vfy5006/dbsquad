c4460acb13a5c35ef50fadaf5f34a3a39585bcdb
mgthoele <58787888+mgthoele@users.noreply.github.com>
2019-12-13 14:36:48 -0700

    Update queries.sql

---
+++ queries.sql
@@ -0,0 +1,16 @@
+//Who is working on projects for the customer Hotel Coral Exxex?
+
+Select EMPLOYEE.NAME, EMPLOYEE.TITLE
+FROM EMPLOYEE, PROJECT, CUSTOMER, ASSINGED_TO
+WHERE EMPLOYEE.ID = ASSINGED_TO.EMPLOYEE AND
+ ASSINGED_TO.PROJECT = PROJECT.ID AND
+ PROJECT.CUSTOMER = CUSTOMER.ID  AND
+CUSTOMER.BUSINESS_NAME = 'Hotel Coral Essex';
+
+Paychecks need to be cut for salaried employees for the month.  List Names, ID's, and check amounts for salaried employees.   
+
+SELECT ID, name,  (pay_rate / 12)
+FROM Employee
+Where pay_class = 'YR'
+Order by 3
+
