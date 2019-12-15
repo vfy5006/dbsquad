0b21a3c2d5751dc89fea8baadf082b78b581c10c
Vincent Young <vy@Vincents-MacBook-Pro.local>
2019-12-15 14:32:19 -0800

    Cleaned up files and added remaining questions with one answered

--- queries.sql
+++ queries.sql
@@ -1,38 +1,64 @@
-/*Who is working on projects for the customer Hotel Coral Exxex?*/
+/* Who is working on projects for the customer Hotel Coral Exxex? */
 
-Select EMPLOYEE.NAME, EMPLOYEE.TITLE
+SELECT EMPLOYEE.NAME, EMPLOYEE.TITLE
 FROM EMPLOYEE, PROJECT, CUSTOMER, ASSINGED_TO
 WHERE EMPLOYEE.ID = ASSINGED_TO.EMPLOYEE AND
  ASSINGED_TO.PROJECT = PROJECT.ID AND
  PROJECT.CUSTOMER = CUSTOMER.ID  AND
 CUSTOMER.BUSINESS_NAME = 'Hotel Coral Essex';
 
-/*Paychecks need to be cut for salaried employees for the month.  List Names, ID's, and check amounts for salaried employees. */  
 
-SELECT ID, name,  (pay_rate / 12)
-FROM Employee
-Where pay_class = 'YR'
-Order by 3
+/* Paychecks need to be cut for salaried EMPLOYEEs for the month.  List NAMEs, ID's, AND check amounts for salaried EMPLOYEEs. */  
 
-/*What is the name of our latest hire?*/
+SELECT ID, NAME,  (PAY_RATE / 12)
+FROM EMPLOYEE
+WHERE PAY_CLASS = 'YR'
+ORDER BY 3;
 
-Select employee."NAME"
-From employee
-where Employee.HIRE_DATE =
+
+/* What is the NAME of our latest hire? */
+
+SELECT EMPLOYEE.NAME
+FROM EMPLOYEE
+WHERE EMPLOYEE.HIRE_DATE =
 (SELECT  MAX(EMPLOYEE.HIRE_DATE)
-FROM EMPLOYEE)
+FROM EMPLOYEE);
 
-/*Display the Cost and components needed for a ENTRDMS service.*/
 
-Select Component.ID, Component.type, component.cost
-From COMPONENT, MADE_OF, HARDWARE, SERVICE
-WHERE  component.id = made_of.component and 
-made_of.hardware = hardware.id and
-service.hardware = hardware.id and 
-service.CODE = 'ENTRDMS'
+/* Display the Cost AND COMPONENTs needed for a ENTRDMS SERVICE. */
 
-/*A snow storm is about to Hit Denver, we need to call all of our Denver employees.  Whar are thier names and phone numbers?*/
+SELECT COMPONENT.ID, COMPONENT.TYPE, COMPONENT.COST
+FROM COMPONENT, MADE_OF, HARDWARE, SERVICE
+WHERE  COMPONENT.ID = MADE_OF.COMPONENT AND 
+MADE_OF.HARDWARE = HARDWARE.ID AND
+SERVICE.HARDWARE = HARDWARE.ID AND 
+SERVICE.CODE = 'ENTRDMS';
 
-Select Employee.name, Employee.phone
-From employee
-Where employee.address like '%Denver%'
+
+/* A snow storm is about to Hit Denver, we need to call all of our Denver EMPLOYEEs.  What are thier NAMEs AND phone numbers? */
+
+SELECT EMPLOYEE.NAME, EMPLOYEE.PHONE
+FROM EMPLOYEE
+WHERE EMPLOYEE.ADDRESS LIKE '%Denver%';
+
+
+/* What are the total earnings for employee D Isiah T. Billings-Clyde? */
+
+SELECT EMPLOYEE.PAY_RATE * 
+(SELECT SUM(HOURS_WORKED) FROM TIMESHEET WHERE TIMESHEET.EMPLOYEE = (SELECT EMPLOYEE.ID FROM EMPLOYEE WHERE EMPLOYEE.NAME = 'D Isiah T. Billings-Clyde')) 
+FROM EMPLOYEE WHERE EMPLOYEE.NAME = 'D Isiah T. Billings-Clyde';
+
+
+/* How much has employee D Isiah T. Billings-Clyde earn for project 00308? */
+
+
+
+/* How much has employee D Isiah T. Billings-Clyde earned worked for Hotel Coral Essex? */
+
+
+
+/* How much money has been paid out to employees who have worked for the customer Hotel Coral Essex? */
+
+
+
+/* How many employees, whose manager's make more than $125000, have worked for Fractal Inc.? */
